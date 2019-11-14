1a298d7d6ccb4ab21d2f052e6bc2b336a564cadf
Mukhammed Ismailov <mukhammed.ismailov@pluto.tv>
2019-11-14 16:35:41 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bh24-app',
+    templateUrl: './app.component.html'
+})
+
+export class AppComponent { }
---
+++ src/app/content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bh24-content',
+    templateUrl: './content.component.html'
+})
+
+export class ContentComponent { }
